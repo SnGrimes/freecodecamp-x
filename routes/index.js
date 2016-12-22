cc1cf8aa1158f319aa697e5c6ba23536b79e95f4
Joshua <joshuacode16@gmail.com>
2016-12-21 23:25:13 -0800

    Added job page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Free Code Camp Dallas | Learn To Code' });
 });
+/* GET job board. */
+router.get('/jobs', function(req, res) {
+     res.render('jobs', { title: 'Free Code Camp Dallas | Learn To Code' });
+});
 
 module.exports = router;
